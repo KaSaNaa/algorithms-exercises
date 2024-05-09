e4b899054118df861940d813d464ec88dbee6bd7
KaSaNaa <pasindudeshitha123@gmail.com>
2024-05-09 21:35:38 +0530

    Optimize bubble sort algorithm

--- src/Sort.js
+++ src/Sort.js
@@ -5,16 +5,8 @@ import { App, snapshot, done, clear } from "./sort-visualizer";
 import "./sort.css";
 
 function sort(nums) {
-  let arrayLength = nums.length;
-  for (let i = 0; i < arrayLength - 1; i++) {
-    for (let j = 0; j < arrayLength - i - 1; j++) {
-      if (nums[j] > nums[j + 1]) {
-        let temp = nums[j];
-        nums[j] = nums[j + 1];
-        nums[j + 1] = temp;
-      }
-    }
-  }
+  
+  
   snapshot(nums);
 }
 
