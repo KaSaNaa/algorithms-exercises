fb5d16c44d3460f54fc65f5a0f7f8493612a03f0
KaSaNaa <pasindudeshitha123@gmail.com>
2024-05-09 23:48:49 +0530

    refactor: Improve efficiency and readability of insertion sort algorithm

--- src/Sort.js
+++ src/Sort.js
@@ -8,16 +8,15 @@ function sort(nums) {
   for (let i = 1; i < nums.length; i++) {
     const element = nums[i];
     let n = i - 1;
-    
-    while (n >= 0 && nums[n] > element) {
-        nums[n + 1] = nums[n];
-        n--;
+
+    while (n >= 0 && nums[n] > element ) {
+      nums[n + 1] = nums[n];
+      n--;
     }
-    nums[n + 1] = element;
-}
-snapshot(nums);
-console.log(nums);
-return nums;
+    nums[n + 1] = element; 
+  }
+  snapshot(nums);
+  return nums;
 }
 
 export default function SortComponent() {
