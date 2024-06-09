0e0706db207a9481372fa558f9eb8281ef78dba8
KaSaNaa <pasindudeshitha123@gmail.com>
2024-06-09 17:53:49 +0530

    feat: Add nestedAdd function for recursively summing nested arrays
    The nestedAdd function has been added to the nested-arrays.test.js file. This function recursively sums all the elements in a nested array and returns the total sum.

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -9,13 +9,26 @@
  
  */
 
-function nestedAdd(array) {
-  // write code here
+export function nestedAdd(array) {
+  let total = 0;
+  for (let index = 0; index < array.length; index++) {
+    let temp = array[index]
+
+    if (Array.isArray(temp)) {
+      total += nestedAdd(temp);
+    } else {
+      total += temp;
+    }
+  }
+  console.log("final: "+ total);
+  return total;
 }
 
+/*
 test.skip("nested arrays addition", () => {
   expect(nestedAdd([1, 2, 3])).toEqual(6);
   expect(nestedAdd([1, [2], 3])).toEqual(6);
   expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5);
   expect(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11])).toEqual(94);
 });
+  */
